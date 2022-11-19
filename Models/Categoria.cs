ab67d328a2623279b440f50645cd0765279bca2d
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-18 22:59:45 -0300

    Restricciones de mapeo

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace entitiyframework.Models;
 
 public class Categoria
 {
+    [Key]
     public Guid CategoriaId { get;set;}
+    [Required]
+    [MaxLength(50)]
     public string Nombre {get;set;}
     public string Descripcion {get;set;}
     public virtual ICollection<Tarea> Tareas {get;set;}
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace entitiyframework.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId { get;set;}
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get;set;}
+    [Required]
+    [MaxLength(50)]
     public string Titulo {get;set;}
     public string Descripcion {get;set;}
     public Prioridad PrioridadTarea {get;set;}
     public DateTime FechaCreacion {get;set;}
     public virtual Categoria Categoria {get;set;}
+    [NotMapped]
+    public string Resumen {get;set;}
 }
 
 public enum Prioridad
