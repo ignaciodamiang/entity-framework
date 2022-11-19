439073615dc99a5cff87128b1073009ad443fc8c
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-19 13:48:45 -0300

    String connection in appsettings.json

--- Program.cs
+++ Program.cs
@@ -4,21 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Para que deje de crear la DB en memoria y no haya conflicto con la creación y conexión a la DB en SQL Server
-// No se debería realizar mas de una config por contexto.
 // builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
-
-// String de conexión para usar EF con Sql Server y se conecte a BD
-// TrustServerCertificate=true; es para que confie en cualquier certificado de servidor (no recomendado en producción)
-// https://stackoverflow.com/questions/17615260/the-certificate-chain-was-issued-by-an-authority-that-is-not-trusted-when-conn
-// 
-builder.Services.AddSqlServer<TareasContext>("Data Source=W10DH;Initial Catalog=TareasDB;user id=sa;password=.; TrustServerCertificate=True;");
+// Se acceder a appsettings.json para obtener la cadena de conexión
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-// Se reutiliza el endpoint y deberíamos poder conectarnos a Sql Server y generarse la base de datos si no está generada.
+
 app.MapGet("/dbconnection", async ([FromServices] TareasContext dbContext) => 
 {
     dbContext.Database.EnsureCreated();
