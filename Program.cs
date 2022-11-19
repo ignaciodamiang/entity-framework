89b1d493e35336120c344f9d9c27f325550083cb
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-19 12:11:58 -0300

    Verifica si EF puede crear bdd
    Crea bdd en memoria y verifica si esta corriendo.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using entitiyframework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
